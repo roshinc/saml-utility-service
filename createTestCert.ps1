11e445aeecbe41b0ca0045ab67fc792bee823cda
Roshin C <idroshin2010@gmail.com>
2024-10-27 22:41:13 -0400

    Update keytool command with new alias and keystore
    Changed the keytool alias from 'mykeyalias' to 'provider2Alias' and updated the keystore and password to 'provider2KeyStore.jks' and 'provider2Password', respectively. This ensures the identifiers are accurate for provider2's environment.

--- createTestCert.ps1
+++ createTestCert.ps1
@@ -1,2 +1,2 @@
 #keytool -genkeypair -alias samlkey -keyalg RSA -keysize 2048 -sigalg SHA256withRSA -validity 365 -keystore samlkeystore.jks -storepass your_keystore_password
-keytool -genkeypair -alias mykeyalias -keyalg RSA -keysize 2048 -keystore test-keystore.jks -storepass changeit -validity 3650 -dname "CN=Test, OU=Test, O=Test, L=Test, S=Test, C=US"
+keytool -genkeypair -alias provider2Alias -keyalg RSA -keysize 2048 -keystore provider2KeyStore.jks -storepass provider2Password -validity 3650 -dname "CN=Test, OU=Test, O=Test, L=Test, S=Test, C=US"
